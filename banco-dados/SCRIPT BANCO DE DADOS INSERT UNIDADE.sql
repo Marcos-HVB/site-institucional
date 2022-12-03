27ea13c262362a9dd31efbe3195d5d2a0ce5e9a5
Marcos-HVB <marcos.boas@sptech.school>
2022-12-03 20:22:19 -0300

    alterações visuais e identação do script

--- banco-dados/SCRIPT BANCO DE DADOS INSERT UNIDADE.sql
+++ banco-dados/SCRIPT BANCO DE DADOS INSERT UNIDADE.sql
@@ -1,101 +1,102 @@
-create database oncotech;
+create database if not exists oncotech;
 
 use oncotech;
 
-create table unidade(
-idUnidade int primary key auto_increment,
-empresa varchar(100),
-nomeUnidade varchar(45),
-rua varchar(45),
-numero varchar(45),
-bairro varchar(45),
-cep char(8),
-complemento varchar(45),
-cnpj char(14),
-fkMatriz int,
-foreign key (fkMatriz) references unidade(idUnidade)
+CREATE TABLE IF NOT EXISTS unidade (
+    idUnidade INT PRIMARY KEY AUTO_INCREMENT,
+    empresa VARCHAR(100),
+    nomeUnidade VARCHAR(45),
+    rua VARCHAR(45),
+    numero VARCHAR(45),
+    bairro VARCHAR(45),
+    cep CHAR(8),
+    complemento VARCHAR(45),
+    cnpj CHAR(14),
+    fkMatriz INT,
+    FOREIGN KEY (fkMatriz)
+        REFERENCES unidade (idUnidade)
 );
 
 
-create table setor(
-idSetor int auto_increment,
-nomeSetor varchar(45),
-localSetor varchar(45),
-fkUnidade int,
-fkRemedio int,
-foreign key (fkRemedio) references remedio(idremedio),
-foreign key (fkUnidade) references unidade(idUnidade),
-primary key (idSetor,fkUnidade)
+CREATE TABLE IF NOT EXISTS remedio (
+    idRemedio INT AUTO_INCREMENT,
+    nomeRemedio VARCHAR(45),
+    tempMin DECIMAL(5 , 2 ),
+    tempMax DECIMAL(5 , 2 )
+);
+
+insert into remedio values
+	(null, 'Talidomida', 15,30),
+    (null, 'Mesilato de Imatinibe', 15,30),
+    (null, 'Dasatinibe', 15,30),
+    (null, 'Rituximabe ', 2,8),
+    (null, 'Trastuzumabe', 2,8),
+    (null, 'Nilotinibe', 15,30),
+    (null, 'Pertuzumabe', 2,8),
+    (null, 'Zidovudina', 15,30);
+
+CREATE TABLE IF NOT EXISTS setor (
+    idSetor INT AUTO_INCREMENT,
+    nomeSetor VARCHAR(45),
+    localSetor VARCHAR(45),
+    fkUnidade INT,
+    FOREIGN KEY (fkUnidade)
+        REFERENCES unidade (idUnidade),
+    PRIMARY KEY (idSetor , fkUnidade)
 );
 
 
-create table remedio(
-idRemedio int primary key auto_increment,
-nomeRemedio varchar (45),
-quantidade int,
-valor decimal(7,2),
-tempMin decimal(3,1),
-tempMax decimal (3,1)
+CREATE TABLE IF NOT EXISTS usuario (
+    idUsuario INT AUTO_INCREMENT,
+    nome VARCHAR(45),
+    sobrenome VARCHAR(45),
+    cargo VARCHAR(45),
+    email VARCHAR(45),
+    senha VARCHAR(45),
+    fkUnidade INT,
+    FOREIGN KEY (fkUnidade)
+        REFERENCES unidade (idUnidade),
+    PRIMARY KEY (idUsuario , fkUnidade)
 );
 
 
-create table sensor(
-idSensor int,
-nomeSensor varchar(45),
-dthora dateTime,
-fkSetor int,
-fkUnidade int,
-foreign key (fkSetor) references setor(idSetor),
-foreign key (fkUnidade) references unidade(idUnidade),
-primary key (idSensor,fkSetor,fkUnidade) 
+
+CREATE TABLE IF NOT EXISTS sensor (
+    idSensor INT,
+    nomeSensor VARCHAR(45),
+    dthora DATETIME,
+    fkSetor INT,
+    fkUnidade INT,
+    FOREIGN KEY (fkSetor)
+        REFERENCES setor (idSetor),
+    FOREIGN KEY (fkUnidade)
+        REFERENCES unidade (idUnidade),
+    PRIMARY KEY (idSensor , fkSetor , fkUnidade)
 );
 
-create table medida(
-idmedida int primary key,
-temperatura decimal(3,1),
-umidade decimal(3,1));
-
-insert medida values
-(1,12.3,25.1),
-(2,10.0,24.7),
-(3,09.2,23.0),
-(4,11.1,20.1),
-(5,15.8,27.2),
-(6,16.1,28.1),
-(7,14.3,26.1),
-(8,12.3,21.1),
-(9,11.0,22.7);
-
-
-
-select temperatura,umidade,sensor.idsensor,setor.idsetor from medida join relatorio on idmedida=fksensor join  sensor on fksensor=idsensor join setor on sensor.fksetor=setor.idsetor;
-
-create table relatorio(
-fkmedida int,
-fksensor int,
-fksetor int,
-fkunidade int,
-primary key (fkmedida,fksensor,fksetor,fkunidade),
-foreign key (fkmedida) references medida(idmedida),
-foreign key (fksensor) references sensor(idsensor),
-foreign key (fksetor) references setor(idsetor),
-foreign key (fkunidade) references unidade(idunidade));
-
-
-
-
-
-create table usuario(
-idUsuario int auto_increment,
-nome varchar(45),
-sobrenome varchar(45),
-cargo varchar(45),
-email varchar(45),
-senha varchar(45),
-fkUnidade int,
-foreign key (fkUnidade) references unidade(idUnidade),
-primary key (idUsuario, fkUnidade)
+CREATE TABLE IF NOT EXISTS medida (
+    idmedida INT PRIMARY KEY,
+    temperatura DECIMAL(3 , 1 ),
+    umidadade DECIMAL(3 , 1 )
 );
+
+CREATE TABLE IF NOT EXISTS relatorio (
+    fkmedida INT,
+    fksensor INT,
+    fksetor INT,
+    fkunidade INT,
+    PRIMARY KEY (fkmedida , fksensor , fksetor , fkunidade),
+    FOREIGN KEY (fkmedida)
+        REFERENCES medida (idmedida),
+    FOREIGN KEY (fksensor)
+        REFERENCES sensor (idsensor),
+    FOREIGN KEY (fksetor)
+        REFERENCES setor (idsetor),
+    FOREIGN KEY (fkunidade)
+        REFERENCES unidade (idunidade)
+);
+
+
 
 -- INSERT VALUES
 insert into unidade values
@@ -107,14 +108,6 @@ insert into unidade values
 (null,'Eurofarma','Unidade Matriz Tatuape','Mota souza','10','Tatuape','08498754','proximo ao HOSPITAL almeida','04966357511110',null),
 (null,'Medley Indústria Farmacêutica','Unidade Matriz Eng.Goulart','Av. Alameda','545','engenheiro goulart','08401123','Andar de cima a o restaurante','04966357000180',null);
 
-
-
-
-
 select * from usuario;
 select * from unidade;
-
-
-
-
-
+select * from unidade;
