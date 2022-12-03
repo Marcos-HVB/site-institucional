c3bb7dbc8c23795447093c085a6d32c94c95d8ee
thiagopequeno <thiago.castro@sptech.school>
2022-12-03 17:15:47 -0300

    pequenas mundaças

--- site-institucional/src/routes/medidas.js
+++ site-institucional/src/routes/medidas.js
@@ -7,6 +7,10 @@ router.get("/ultimas/:idAquario", function (req, res) {
     medidaController.buscarUltimasMedidas(req, res);
 });
 
+router.post("/cadastrarsensor", function (req, res) {
+    usuarioController.cadastrarsensor(req, res);
+})
+
 router.get("/tempo-real/:idAquario", function (req, res) {
     medidaController.buscarMedidasEmTempoReal(req, res);
 })
